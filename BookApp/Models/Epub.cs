bfd46a66ebf55177aec1c46ee96a8fb97ae1b916
Mads Frandsen <60930334+MadsMBFrandsen@users.noreply.github.com>
2025-01-03 23:09:56 +0100

    addet HasStartNumbers to model epub

--- BookApp/Models/Epub.cs
+++ BookApp/Models/Epub.cs
@@ -21,5 +21,6 @@ namespace BookApp.Models
                 return Chapters?.Sum(chapter => chapter.WordCount) ?? 0;
             }
         }
+        public bool HasStartNumbers { get; set; }
     }
 }
